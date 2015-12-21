00ea0e669212120ec0465ee252d1996ae51d8ac3
Jordan Wallet <JordanW@axosoft.com>
2015-12-21 14:28:12 -0700

    Add help message, prep for more intensive work

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # smashchat-bot
 
 ##Setup:
-In the repo, `npm install --save botkit`
+In the repo, install dependencies because this isn't a proper package:
+`npm install --save botkit`
+`npm install --save lodash`
 
 Create a `token.js` which exports as a string with a valid slack bot token
 
--- smashchat-bot.js
+++ smashchat-bot.js
@@ -1,9 +1,34 @@
 "use strict"
+
 const Botkit = require('botkit');
-const controller = Botkit.slackbot();
+const _ = require('lodash');
+
+const SMASH_CHANNEL_ID = 'G02K3JP83';
+const ADMIN = 'U04CT4Y06' //jordan_wallet
+
+const differentDays = function differentDays(date1, date2) {
+  date1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+  date2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+  const ms = Math.abs(date1-date2);
+  const msPerDay = 1000 * 60 * 60 * 24;
+  return Math.floor(ms/msPerDay);
+}
+
+const controller = Botkit.slackbot({
+  json_file_store: './saveData',
+});
+
 const bot = controller.spawn({
   token: require('./token.js')
 });
+
+bot.privateMessage = function privateMessage(user, message) {
+  bot.api.im.open({ user: user }, function(error, response) {
+    bot.api.chat.postMessage({ as_user: true, channel: response.channel.id, text: message }, function(error, response) {
+      //noop
+    });
+  });
+};
 
 bot.startRTM(function(error, bot, payload) {
   if (error) {
@@ -14,33 +39,43 @@ bot.startRTM(function(error, bot, payload) {
 
 // helpers
 const getScoreMessage = function() {
-  return 'Jose\'s score is _not_ at an all-time high. (2241 vs. 2250)';
+  return 'Jose\'s score is _not_ at an all-time high. (2220 vs. 2250)';
 }
 
+let dededeTimer = 0;
+const getDededeMessage = function() {
+  return 'http://img.ifcdn.com/images/eed2c392ae247b9824dffecd6c56000bd45accd259a9ee1bc9e2497548bc8bb4_1.gif';
+}
 
 // logic
-controller.hears(['^score$'],['direct_message','direct_mention'], function(bot,message) {
+controller.hears(['^score$'],['direct_message', 'direct_mention'], function(bot,message) {
   bot.reply(message, getScoreMessage());
 });
 
-controller.hears(['.*'],['direct_message','direct_mention'], function(bot, message) {
-  bot.startConversation(message, function(error, convo) {
-    convo.ask('I don\'t understand. Did you mean `score`?', [
-      {
-        pattern: new RegExp('^score$|' + bot.utterances.yes.source),
-        callback: function(response,convo) {
-          convo.say(getScoreMessage());
-          convo.next();
-        }
-      },
-      {
-        default: true,
-        callback: function(response, convo) {
-          convo.say('Yes you did.');
-          convo.say('Jose\'s score is _not_ at an all-time high. (2241 vs. 2250)');
-          convo.next();
-        }
-      }
-    ]);
   })
+});
+controller.hears(['dedede', 'daniel'],['direct_mention', 'mention', 'ambient'], function(bot,message) {
+  if (dededeTimer) {
+    dededeTimer = Math.max(dededeTimer - 2, 0);
+    return;
+  }
+  bot.reply(message, getDededeMessage());
+  dededeTimer = 50;
+});
+
+controller.hears(['help'],['direct_message', 'direct_mention'], function(bot, message) {
+  bot.privateMessage(message.user, 'Here are my commands:\n'
+    + '  `help` This list of commands.\n'
+    + '  `score` Find out if Jose\'s score is at an all-time high.' //'\n'
+  );
+});
+
+controller.hears(['.*'],['direct_message', 'direct_mention'], function(bot, message) {
+  bot.reply(message, 'I don\'t understand. Try messaging me for `help`.');
+});
+
+controller.on('message_received', function(bot, message) {
+  if (dededeTimer) {
+    dededeTimer--;
+  }
 });
